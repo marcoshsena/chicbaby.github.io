6323eb0a4bb0fde35c9671fb399d81ff0d27ae88
marcoshsena <168047441+marcoshsena@users.noreply.github.com>
2026-01-29 10:18:50 -0300

    Refactor comprarProduto to open URL directly
    Simplified product purchase function by removing button click event listeners and directly opening a new tab.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,6 @@
 function comprarProduto(id, nome, url) {
   // 1. Abre o produto da loja em nova aba
-  document.querySelectorAll('.btn-comprar').forEach(btn => {
-    btn.addEventListener('click', () => {
-      const url = btn.dataset.url;
-  
-      if (!url) {
-        console.error('URL não encontrada para o botão');
-        return;
-      }
-  
-      window.open(url, '_blank', 'noopener,noreferrer');
-    });
-  });
-
+  window.open(undefined, '_blank')
 
   // 2. Redireciona para a página de confirmação
   const confirmacaoUrl =
