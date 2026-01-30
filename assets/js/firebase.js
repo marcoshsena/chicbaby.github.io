7fe6136da69d91b5dae512b071def439df7e6a48
marcoshsena <168047441+marcoshsena@users.noreply.github.com>
2026-01-30 09:53:19 -0300

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,7 +1,10 @@
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJETO",
+  apiKey: "AIzaSyDprcpxXqkgCbfyVqN4_MHA1RPOsBpmeEo",
+  authDomain: "chicbaby-3aae8.firebaseapp.com",
+  projectId: "chicbaby-3aae8",
+  storageBucket: "chicbaby-3aae8.firebasestorage.app",
+  messagingSenderId: "746843032499",
+  appId: "1:746843032499:web:796a7d976050325ed119c6"
 };
 
 firebase.initializeApp(firebaseConfig);
