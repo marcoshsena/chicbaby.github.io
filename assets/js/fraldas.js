a87ccaaf390cbc3715d76bea83acd9bd8aed1b45
marcoshsena <168047441+marcoshsena@users.noreply.github.com>
2026-01-30 08:40:16 -0300

    Update fraldas.js

--- assets/js/fraldas.js
+++ assets/js/fraldas.js
@@ -1,3 +1,7 @@
+<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore-compat.js"></script>
+<script src="assets/js/firebase.js"></script>
+
 document.addEventListener('DOMContentLoaded', () => {
   const params = new URLSearchParams(window.location.search);
   const tamanhoSelecionado = params.get('tamanho');
