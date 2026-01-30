bfdc7ac53e00f16a27d45e7b3d3b4d548ace62d3
marcoshsena <168047441+marcoshsena@users.noreply.github.com>
2026-01-30 08:40:27 -0300

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,7 @@
+<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore-compat.js"></script>
+<script src="assets/js/firebase.js"></script>
+
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('.btn-ver-ofertas').forEach(btn => {
     btn.addEventListener('click', () => {
